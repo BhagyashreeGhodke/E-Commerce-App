d062eee383856b65a132623ffb9c8dd3df798d00
BhagyashreeGhodke <drbpghodke@gmail.com>
2024-05-16 21:04:56 +0530

    create product controller

--- backend/src/controllers/product.controller.js
+++ backend/src/controllers/product.controller.js
@@ -1,38 +1,42 @@
-import mongoose from 'mongoose';
-import fs from 'fs';
-import Product from '../models/product.model.js'; // Adjust the path as necessary
+import { asyncHandler } from "../utils/asyncHandler.js";
+import Product from "../models/product.model.js";
 
-async function insertProducts() {
+const getProducts = asyncHandler(async (req, res) => {
     try {
-        // Read the CSV file
-        const csvData = fs.readFileSync('products.csv', 'utf-8');
+        const products = await Product.find({});
 
-        // Parse the CSV data
-        const rows = csvData.split('\n').slice(1); // Skip header row
-
-        // Convert CSV rows to product objects
-        const batchSize = 100; // Adjust batch size as needed
-        for (let i = 0; i < rows.length; i += batchSize) {
-            const batch = rows.slice(i, i + batchSize);
-            const products = batch.map(row => {
-                const [url, detailUrl, shortTitle, longTitle, mrp, cost, priceDiscount, quantity, description, discount, tagline] = row.split(',');
-                return {
-                    url, detailUrl, shortTitle, longTitle,
-                    price: { mrp: parseFloat(mrp) || 0, cost: parseFloat(cost), discount: priceDiscount || 0},
-                    quantity: parseInt(quantity),
-                    title: { shortTitle, longTitle }, // Assuming title is an object with shortTitle and longTitle
-                    description, discount, tagline
-                };
-            });
-            await Product.insertMany(products);
-            console.log(`Inserted batch ${i / batchSize + 1}`);
+        if(!products) {
+            throw new ApiError(404, "no productfound");
         }
-        console.log('Products inserted successfully');
-    } catch (error) {
-        console.error('Error inserting products:', error);
-    } finally {
-        mongoose.disconnect();
+
+        res.json(products);
+        
+    }catch (error) {
+        console.error("error: ", error)
     }
 }
+)
 
-export default insertProducts;
+const getProductById = asyncHandler(async (req, res) => {
+    try {
+        if (!req.params.id) {
+            throw new ApiError(404, "no id found");
+        }
+        const products = await Product.findOne({ '_id': request.params.id });
+
+        if (!products) {
+            throw new ApiError(404, "no product found");
+        }
+
+        res.json(products);
+        
+    }catch (error) {
+        console.error("error: ", error)
+    }
+}
+)
+
+export {
+    getProducts,
+    getProductById
+}
